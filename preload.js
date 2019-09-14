485377801ce02bd3f5e381919248458226bdc7c9
Haaayden <brooklyn1984@qq.com>
2019-09-14 18:37:46 +0800

    feat(config): 针对 utools 调整配置

--- preload.js
+++ preload.js
@@ -1,6 +1,16 @@
 console.log('=== utools Object ===', utools)
-console.log('=== 图片路径 ===', utools.getPath('exe'))
 
-utools.onPluginEnter(function(data) {
+utools.onPluginEnter(({ code, payload, type }) => {
   console.log('=== plugin enter ===', data)
+  // 图片加阴影
+  if (code === 'default') {
+    // 上传的是图片文件  文件路径
+    if (type === 'files') {
+      const imgPath = payload[0].path
+      const imgName = path.name
+    } else if (type === 'img') {
+      // 粘贴板图片   base64
+      const imgBase64 = payload
+    }
+  }
 })
